8e3aa13c5518412012b32198713c3152ab2ba50e
Nurullo <khuwaidullaev@gmail.com>
2021-07-20 13:46:17 +0500

    Using Switch and exact for Configuring Routes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Products() {
   return (
     <section>
       <h1>The Products Page.</h1>
       <ul>
-        <li>House</li>
-        <li>Book</li>
-        <li>TV</li>
-        <li>Sofa</li>
-        <li>Swimming pool</li>
+        <li>
+          <Link to="/products/p1">House</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">TV</Link>
+        </li>
+        <li>
+          <Link to="/products/p4">Sofa</Link>
+        </li>
+        <li>
+          <Link to="/products/p5">Swimming pool</Link>
+        </li>
       </ul>
     </section>
   );
